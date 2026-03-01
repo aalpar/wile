01a93dcc4577f34df8d41a817aea68a2b80b8b36
Aaron Alpar <aaron.alpar@gmail.com>
2026-02-28 23:19:11 -0800

    additional todo items and machine context

--- machine/machine_context.go
+++ machine/machine_context.go
@@ -815,19 +815,19 @@ func (p *MachineContext) Run() error {
 			}
 			tup, ok := v.(values.Tuple)
 			if !ok {
-				return values.WrapForeignErrorf(values.ErrNotAList,
-					"apply: final argument must be a list, got %s", v.SchemeString())
+				return applyCallableError(mc, values.WrapForeignErrorf(values.ErrNotAList,
+					"apply: final argument must be a list, got %s", v.SchemeString()))
 			}
 			sentinel, err := tup.ForEach(mc.ctx, func(_ context.Context, _ int, _ bool, elem values.Value) error {
 				mc.evals.Push(elem)
 				return nil
 			})
 			if err != nil {
-				return err
+				return applyCallableError(mc, err)
 			}
 			if !values.IsEmptyList(sentinel) {
-				return values.WrapForeignErrorf(values.ErrNotAList,
-					"apply: final argument is an improper list")
+				return applyCallableError(mc, values.WrapForeignErrorf(values.ErrNotAList,
+					"apply: final argument is an improper list"))
 			}
 			mc.pc++
 
