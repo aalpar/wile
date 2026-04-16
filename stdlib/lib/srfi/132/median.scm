e8cef8eb6ecb1f7e3eb64e4adb9cdb468caf7ce2
Aaron Alpar <aaron.alpar@gmail.com>
2026-04-15 18:53:52 -0700

    feat(srfi-132): dedup, selection, and median
    - dedup.scm: neighbor dedup for lists (fresh/destructive) and vectors
      (compact copy / in-place returning end index)
    - select.scm: quickselect with median-of-3 pivot, three-way partition;
      vector-select! returns k-th smallest, vector-separate! partitions
    - median.scm: built on quickselect; even-length scans for upper middle;
      default mean returns lesser element per spec

--- stdlib/lib/srfi/132/median.scm
+++ stdlib/lib/srfi/132/median.scm
@@ -1,9 +1,111 @@
 ;; median.scm -- median finding for vectors
 ;; Part of SRFI 132: Sort Libraries
-;; Stub: will be implemented in a later task.
 
-(define (vector-find-median less? v knil . rest)
-  (error "vector-find-median: not yet implemented"))
+(define (%default-mean a b)
+  "Internal: default mean procedure for vector-find-median when no
+mean is provided. Returns the lesser of the two middle elements,
+per SRFI 132 specification."
+  a)
 
-(define (vector-find-median! less? v knil . rest)
-  (error "vector-find-median!: not yet implemented"))
+(define (%vector-find-median! < v knil mean)
+  "Internal: find the median of V, which may be mutated.
+Uses vector-select! for O(n) expected time."
+  (let ((len (vector-length v)))
+    (cond
+      ((= len 0)
+       knil)
+      ((= len 1)
+       (vector-ref v 0))
+      ((odd? len)
+       (vector-select! < v (quotient len 2)))
+      (else
+       ;; Even length: find the two middle elements.
+       ;; k is the index of the lower middle.
+       (let ((k (- (quotient len 2) 1)))
+         (let ((lower (vector-select! < v k)))
+           ;; After quickselect, v[k] is correct, but v[k+1..len)
+           ;; is not necessarily sorted. Scan for the minimum of
+           ;; v[k+1..len) to find the upper middle.
+           (let ((upper (vector-ref v (+ k 1))))
+             (let scan ((i (+ k 2)) (m upper))
+               (if (>= i len)
+                   (mean lower m)
+                   (let ((vi (vector-ref v i)))
+                     (if (< vi m)
+                         (scan (+ i 1) vi)
+                         (scan (+ i 1) m))))))))))))
+
+(define vector-find-median!
+  (case-lambda
+    ((< v knil)
+     "Find the median of vector V according to the comparison
+procedure <. If V is empty, return KNIL. If V has odd length,
+return the middle element. If V has even length, return the result
+of applying the mean procedure (default: return the lesser of the
+two middle elements) to the two middle elements.
+
+The vector V may be mutated as a side effect of the selection
+algorithm.
+
+An optional fourth argument MEAN is a two-argument procedure used
+to compute the mean of the two middle elements for even-length
+vectors. When omitted, the lesser middle element is returned.
+
+Examples:
+  (vector-find-median! < (vector 3 1 4 1 5) 0)    => 3
+  (vector-find-median! < (vector 3 1 4 1 5 9) 0)   => 3
+  (vector-find-median! < (vector 3 1 4 1 5 9) 0
+    (lambda (a b) (/ (+ a b) 2)))                   => 7/2
+  (vector-find-median! < (vector) 42)               => 42
+  (vector-find-median! < (vector 7) 0)              => 7
+
+Parameters:
+  < : procedure -- a two-argument comparison (less-than) predicate
+  v : vector (mutated)
+  knil : value -- returned when v is empty
+  mean : procedure (optional) -- two-argument mean procedure
+Returns: value -- the median or mean of two middle elements
+Category: srfi-132
+Keywords: median, middle, order statistic, select, vector
+
+See also: `vector-find-median', `vector-select!'."
+     (%vector-find-median! < v knil %default-mean))
+    ((< v knil mean)
+     (%vector-find-median! < v knil mean))))
+
+(define vector-find-median
+  (case-lambda
+    ((< v knil)
+     "Find the median of vector V without modifying V. Makes a copy
+of V and delegates to vector-find-median!.
+
+If V is empty, return KNIL. If V has odd length, return the middle
+element. If V has even length, return the result of applying the
+mean procedure (default: return the lesser of the two middle
+elements) to the two middle elements.
+
+An optional fourth argument MEAN is a two-argument procedure used
+to compute the mean of the two middle elements for even-length
+vectors. When omitted, the lesser middle element is returned.
+
+Examples:
+  (vector-find-median < #(3 1 4 1 5) 0)    => 3
+  (vector-find-median < #(3 1 4 1 5 9) 0)   => 3
+  (vector-find-median < #(3 1 4 1 5 9) 0
+    (lambda (a b) (/ (+ a b) 2)))            => 7/2
+  (vector-find-median < #() 42)              => 42
+  (vector-find-median < #(7) 0)              => 7
+
+Parameters:
+  < : procedure -- a two-argument comparison (less-than) predicate
+  v : vector (not modified)
+  knil : value -- returned when v is empty
+  mean : procedure (optional) -- two-argument mean procedure
+Returns: value -- the median or mean of two middle elements
+Category: srfi-132
+Keywords: median, middle, order statistic, select, non-destructive, vector
+
+See also: `vector-find-median!', `vector-select!'."
+     (vector-find-median! < (vector-copy v) knil %default-mean))
+    ((< v knil mean)
+     (vector-find-median! < (vector-copy v) knil mean))))
--- stdlib/lib/srfi/132/select.scm
+++ stdlib/lib/srfi/132/select.scm
@@ -1,9 +1,138 @@
 ;; select.scm -- selection and separation for vectors
 ;; Part of SRFI 132: Sort Libraries
-;; Stub: will be implemented in a later task.
 
-(define (vector-select! less? v k . rest)
-  (error "vector-select!: not yet implemented"))
+(define (%swap! v i j)
+  "Internal: swap elements at indices I and J in vector V."
+  (let ((tmp (vector-ref v i)))
+    (vector-set! v i (vector-ref v j))
+    (vector-set! v j tmp)))
 
-(define (vector-separate! less? v k . rest)
-  (error "vector-separate!: not yet implemented"))
+(define (%median-of-3 < v a b c)
+  "Internal: return the index of the median of v[a], v[b], v[c]
+according to the comparison predicate <."
+  (let ((va (vector-ref v a))
+        (vb (vector-ref v b))
+        (vc (vector-ref v c)))
+    (if (< va vb)
+        (if (< vb vc)
+            b
+            (if (< va vc)
+                c
+                a))
+        (if (< va vc)
+            a
+            (if (< vb vc)
+                c
+                b)))))
+
+(define (%partition3! < v lo hi pivot)
+  "Internal: three-way partition v[lo..hi) around PIVOT.
+Rearranges elements so that:
+  v[lo..lt-end)    < pivot
+  v[lt-end..eq-end) = pivot (neither < nor >)
+  v[eq-end..hi)    > pivot
+Returns two values: lt-end and eq-end."
+  (let loop ((i lo) (lt lo) (gt hi))
+    (if (>= i gt)
+        (values lt gt)
+        (let ((vi (vector-ref v i)))
+          (cond
+            ((< vi pivot)
+             (%swap! v i lt)
+             (loop (+ i 1) (+ lt 1) gt))
+            ((< pivot vi)
+             (%swap! v i (- gt 1))
+             (loop i lt (- gt 1)))
+            (else
+             (loop (+ i 1) lt gt)))))))
+
+(define (%quickselect! < v k lo hi)
+  "Internal: rearrange v so that v[k] is the k-th smallest
+element of v[lo..hi). Uses quickselect with median-of-3 pivot."
+  (when (>= (- hi lo) 2)
+    (let* ((mid (+ lo (quotient (- hi lo) 2)))
+           (pivot-idx (%median-of-3 < v lo mid (- hi 1)))
+           (pivot (vector-ref v pivot-idx)))
+      (call-with-values
+        (lambda ()
+          (%partition3! < v lo hi pivot))
+        (lambda (lt-end eq-end)
+          (cond
+            ((< k lt-end)
+             (%quickselect! < v k lo lt-end))
+            ((>= k eq-end)
+             (%quickselect! < v k eq-end hi))))))))
+
+(define vector-select!
+  (case-lambda
+    ((< v k)
+     "Find the K-th smallest element in vector V according to the
+comparison procedure <. The vector is mutated as a side effect of
+the selection algorithm (quickselect with median-of-3 pivot).
+K is zero-based relative to the subrange.
+
+Optional START and END arguments restrict the selection to a
+subrange of V.
+
+Examples:
+  (let ((v (vector 5 3 1 4 2)))
+    (vector-select! < v 0))   => 1
+  (let ((v (vector 5 3 1 4 2)))
+    (vector-select! < v 2))   => 3
+  (let ((v (vector 5 3 1 4 2)))
+    (vector-select! < v 4))   => 5
+  (let ((v (vector 9 8 7 6 5 4 3 2 1)))
+    (vector-select! < v 1 2 7))  => 4
+
+Parameters:
+  < : procedure -- a two-argument comparison (less-than) predicate
+  v : vector (mutated)
+  k : integer -- zero-based rank within the subrange
+  start : integer (optional, default 0)
+  end : integer (optional, default (vector-length v))
+Returns: value -- the k-th smallest element
+Category: srfi-132
+Keywords: select, kth, quickselect, order statistic, vector
+
+See also: `vector-separate!', `vector-find-median!', `vector-sort!'."
+     (%quickselect! < v k 0 (vector-length v))
+     (vector-ref v k))
+    ((< v k start)
+     (%quickselect! < v (+ start k) start (vector-length v))
+     (vector-ref v (+ start k)))
+    ((< v k start end)
+     (%quickselect! < v (+ start k) start end)
+     (vector-ref v (+ start k)))))
+
+(define vector-separate!
+  (case-lambda
+    ((< v k)
+     "Rearrange V so that the K smallest elements (according to <)
+are in positions 0 through K-1 (in no particular order among
+themselves). This is equivalent to a partial sort. K is zero-based
+relative to the subrange.
+
+Optional START and END arguments restrict the operation to a
+subrange of V.
+
+Examples:
+  (let ((v (vector 5 3 1 4 2)))
+    (vector-separate! < v 2)
+    (vector-ref v 0))  ;; one of 1 or 2
+
+Parameters:
+  < : procedure -- a two-argument comparison (less-than) predicate
+  v : vector (mutated)
+  k : integer -- number of smallest elements to place at the front
+  start : integer (optional, default 0)
+  end : integer (optional, default (vector-length v))
+Returns: unspecified
+Category: srfi-132
+Keywords: separate, partition, kth, quickselect, vector
+
+See also: `vector-select!', `vector-sort!'."
+     (%quickselect! < v k 0 (vector-length v)))
+    ((< v k start)
+     (%quickselect! < v (+ start k) start (vector-length v)))
+    ((< v k start end)
+     (%quickselect! < v (+ start k) start end))))
